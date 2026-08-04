2e6303d83944775788df6ce1ae17195663f13317
Harish Verma <harish97verma@gmail.com>
2026-08-04 07:34:56 +0530

    count vowels

---
+++ easy/countvowels.js
@@ -0,0 +1,16 @@
+
+let str = "amazone";
+
+function countVowels(str) {
+    let count = 0;
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    str = str.toLowerCase();
+    for (let i = 0; i <str.length; i++) {
+        if (vowels.includes(str[i])){
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countVowels(str)); // Output: 3
